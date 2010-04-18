c9b9e58c5c6e940805f798ea87926d24a0fdf7b0
Adriano Bonat <adrianob@gmail.com>
2010-04-18 03:45:51 -0300

    Removed BlockNode, I will work on this later.

--- src/main/java/com/djtemplate4j/BlockNode.java
+++
@@ -1,9 +0,0 @@
-package com.djtemplate4j;
-
-import java.util.Map;
-
-public class BlockNode implements Node {
-    public String render(Map<String, Object> context) {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
-    }
-}
